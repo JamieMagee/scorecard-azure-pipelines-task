cfc25bbfa119b535b024a69f6e5bd8d6fc3bd431
Jamie Magee <jamie.magee@gmail.com>
2024-12-13 10:41:48 -0800

    lint fix

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,18 +1,18 @@
-import { build } from 'esbuild';
-import { copy } from 'esbuild-plugin-copy';
+import { build } from "esbuild";
+import { copy } from "esbuild-plugin-copy";
 
 await build({
-  entryPoints: ['src/index.mts'],
+  entryPoints: ["src/index.mts"],
   bundle: true,
-  platform: 'node',
-  format: 'esm',
-  outfile: 'dist/index.mjs',
+  platform: "node",
+  format: "esm",
+  outfile: "dist/index.mjs",
   minify: true,
   plugins: [
     copy({
       assets: {
-        from: 'src/task.json',
-        to: '.',
+        from: "src/task.json",
+        to: ".",
       },
     }),
   ],
