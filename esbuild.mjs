cba192817db8294512a4404b7c22b1c93910873a
Jamie Magee <jamie.magee@gmail.com>
2024-12-13 11:15:48 -0800

    Make the extension public

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,13 +1,15 @@
 import { build } from "esbuild";
 import { copy } from "esbuild-plugin-copy";
+import fs from "node:fs";
 
-await build({
+const result = await build({
   entryPoints: ["src/index.mts"],
   bundle: true,
   platform: "node",
   format: "esm",
   outfile: "dist/index.mjs",
   minify: true,
+  metafile: true,
   plugins: [
     copy({
       assets: {
@@ -17,3 +19,5 @@ await build({
     }),
   ],
 });
+
+fs.writeFileSync("dist/meta.json", JSON.stringify(result.metafile, null, 2));
